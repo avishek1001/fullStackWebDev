5725dfb45f35189d21a8e7dd7230b05e9f362d30
avishek1001 <1706avsihek@gmail.com>
2024-12-29 06:09:49 +0530

    project upload photobooth

--- FullstackWebDev/PhotoBooth/index.js
+++ FullstackWebDev/PhotoBooth/index.js
@@ -3,24 +3,51 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 let info = [
     {
         username: 'sample',
-        img: '',
-        comment: 'sample comment !!!!!!!!!'
+        img: 'https://static.vecteezy.com/system/resources/thumbnails/006/091/020/small_2x/sample-stamp-in-rubber-style-red-round-grunge-sample-sign-rubber-stamp-on-white-illustration-free-vector.jpg',
+        comments: []
     }
 ]
 
+
 app.get('/', (req, res) => {
-
-    res.render('home', {info});
+    //console.log(info);
+    res.render('home', { info });
 })
 
-app.get('/:username', (req, res) => {
-    res.send('Hello')
+app.get('/upload', (req, res) => {
+    res.render('upload');
 })
 
-app.listen(1234, () =>{
+app.post('/upload', (req, res) => {
+    //console.log(req.body);
+    const {username, img} = req.body;
+    info.push({username, img, comments: []});
+    res.redirect('/');
+})
+
+app.get('/r/:username', (req, res) => {
+    const {username} = req.params;
+    const userInfo = info.find(i => i.username === username);
+    res.render('display', {userInfo});
+})
+
+app.post('/r/:username', (req, res) => {
+    const {username} = req.params;
+    const {comment} = req.body;
+    const userInfo = info.find(c => c.username === username);
+    userInfo.comments.push(comment);
+    res.redirect(`/r/${username}`);
+})
+
+
+
+
+app.listen(1234, () => {
     console.log('Listening from port 1234!!!!')
 })
